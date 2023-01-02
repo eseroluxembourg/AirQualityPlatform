138e7d874a07c1e1c1a5482f19dbce1fca5d946a
Dennis Fink <dennis.fink@science-center.lu>
2023-01-02 09:13:13 +0100

    Add new CO2 sensor test
    This uses the SMBus library to talk with the CozIR-LP3 over I2C.
    
    For this to work, the sensor must be configured to use I2C and be connected to the I2C pins on the RaspberryPi. Look into the datasheet of the sensor on how to do that.
    
    This test routine sends a self-test command (0x50) and only if this self-test works it reads one CO2 reading.

---
+++ AirPlatform/test_new_co2_sensor.py
@@ -0,0 +1,24 @@
+from smbus import SMBus
+
+I2C_CHANNEL = 1
+DEVICE_ADDRESS = 0x41
+SELF_TEST = (0x50, 1)
+READ_CO2 = (0x34, 2)
+
+
+def read_i2c_block(register, num_bytes):
+    bus = SMBus(1)
+    return int.from_bytes(
+        bus.read_i2c_block_data(DEVICE_ADDRESS, register, num_bytes), "big"
+    )
+
+
+def test():
+    if read_i2c_block(SELF_TEST[0], SELF_TEST[1]) == 85:
+        print("CO2 sensor is working properly!")
+        co2_level = read_i2c_block(READ_CO2[0], READ_CO2[1])
+        print("Current CO2 in ppm: {}".format(co2_level))
+
+
+if __name__ == "__main__":
+    test()
